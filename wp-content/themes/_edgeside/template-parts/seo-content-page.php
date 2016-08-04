808f1719a008ab068f2c1ba7200a96c44e94e3b4
MattRijk <matthewrijk@gmail.com>
2016-08-04 15:55:29 +0000

    added project to repo.

---
+++ wp-content/themes/_edgeside/template-parts/seo-content-page.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Template part for displaying page content in page.php.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package _edgeside
+ */
+
+?>
+
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php the_title( '<h1 id="page-title" class="entry-title">', '</h1>' ); ?>
+	</header><!-- .entry-header -->
+
+	<div class="widget-area" role="complementary">
+		<?php dynamic_sidebar('page-1'); ?>
+	</div>
+
+	<div id="entry-content" class="entry-content">
+		<?php
+			the_content();
+
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', '_edgeside' ),
+				'after'  => '</div>',
+			) );
+		?>
+		
+	</div><!-- .entry-content -->
+
+	<?php if ( get_edit_post_link() ) : ?>
+		<footer class="entry-footer">
+			<?php
+				edit_post_link(
+					sprintf(
+						/* translators: %s: Name of current post */
+						esc_html__( 'Edit %s', '_edgeside' ),
+						the_title( '<span class="screen-reader-text">"', '"</span>', false )
+					),
+					'<span class="edit-link">',
+					'</span>'
+				);
+			?>
+		</footer><!-- .entry-footer -->
+	<?php endif; ?>
+</article><!-- #post-## -->
