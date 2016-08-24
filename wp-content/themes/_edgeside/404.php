e89c5030d1e1061332ccd9504d7a0a3e53d0942c
MattRijk <matthewrijk@gmail.com>
2016-08-24 20:41:22 +0000

    added 404 styles

--- wp-content/themes/_edgeside/404.php
+++ wp-content/themes/_edgeside/404.php
@@ -14,46 +14,31 @@ get_header(); ?>
 
 			<section class="error-404 not-found">
 				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', '_edgeside' ); ?></h1>
+					<h1 class="page-title"></h1>
 				</header><!-- .page-header -->
-
+			
 				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', '_edgeside' ); ?></p>
+				<div id="page-content-container">
+					<aside id="404-box-back-home">
+						<div >
+							<img id="" class="" src="https://edgeside001-mattrijk.c9users.io/wp-content/themes/_edgeside/img/404-back-home400x300.svg"/>
+	
+							<a href="/">
+							<h2 id="404-back-home-button">BACK HOME</h2>	
+							</a>
 
-					<?php
-						get_search_form();
-
-						the_widget( 'WP_Widget_Recent_Posts' );
-
-						// Only show the widget if site has multiple categories.
-						if ( _edgeside_categorized_blog() ) :
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', '_edgeside' ); ?></h2>
-						<ul>
-						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-						?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-						endif;
-
-						/* translators: %1$s: smiley */
-						$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', '_edgeside' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-
-						the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
+						</div>
+					</aside>
+					
+					<aside id="404-box-search">
+						<div >
+							<img id="" class="" src="https://edgeside001-mattrijk.c9users.io/wp-content/themes/_edgeside/img/404-search-400x300.svg"/>
+							<div>
+							<?php get_search_form(); ?>
+							</div>
+						</div>
+					</aside>
+				</div>
 				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
 
